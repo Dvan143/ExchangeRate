6a20bb7a6d51e04bee55bfecffb5d0068bdec976
Dvan143 <dvanish445@yandex.ru>
2024-11-28 19:45:53 +0300

    Ended work with Controller & Modifyed index.html & Added rate now

--- src/main/java/org/example/exchangerate/ApiController.java
+++ src/main/java/org/example/exchangerate/ApiController.java
@@ -12,19 +12,33 @@ public class ApiController {
 
     RestTemplate restTemplate = new RestTemplate();
     private final String url = "https://open.er-api.com/v6/latest/";
+    // Обращаемся за курсом и временем для header
+    String UsdReq = restTemplate.getForObject(url+"USD", String.class);
+    String EuroReq = restTemplate.getForObject(url+"EUR", String.class);
+    // Получаем актуальное время курса
+    String time = UsdReq.split("time_last_update_utc\":\"")[1].split(",")[1].split("\\+")[0];
 
+    String usd = UsdReq.split("RUB" + "\":")[1].split(",")[0];
+    String euro = EuroReq.split("RUB" + "\":")[1].split(",")[0];
 
     @GetMapping
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("time", time);
+        model.addAttribute("usd", usd);
+        model.addAttribute("euro",euro);
         return "index";
     }
+
     @GetMapping("/exchange")
     public String exchange(@RequestParam String firstCurrency, @RequestParam String secondCurrency, Model model){
+        model.addAttribute("time", time);
+        model.addAttribute("usd", usd);
+        model.addAttribute("euro",euro);
         // Получаем api запрос
         String req = restTemplate.getForObject(url+firstCurrency, String.class);
-        // Обрабатываем полученный ответ и достаем из него необходимый курс
+        // Обрабатываем получrенный ответ и достаем из него необходимый курс
         String answer = req.split(secondCurrency + "\":")[1].split(",")[0];
         model.addAttribute("answer", answer);
-        return "answer";
+        return "index";
     }
 }
--- src/main/resources/static/script.js
+++
@@ -1,5 +0,0 @@
-function exchange(){
-    const firstCurrency = document.form.firstCurrency.options[0];
-    const secondCurrency = document.form.secondCurrency.options[0];
-    location.href = '/exchange?firstCurrency=' + firstCurrency + '&secondCurrency=' + secondCurrency;
-}
